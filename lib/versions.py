fa69b0926d29622b0e826330ec70ee9e64ae4f39
Chief <brianmwangi10808@gmail.com>
2024-05-03 15:56:51 +0300

    commit

--- lib/versions.py
+++ lib/versions.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 import pytest
+import importlib.metadata as metadata
 
 def python_version():
     return sys.version_info
@@ -9,4 +10,15 @@ def requests_version():
     return requests.__version__
 
 def pytest_version():
-    return pytest.__version__
+    return metadata.version("pytest")
+
+def test_python_version():
+    version_info = python_version()
+    assert version_info.major == 3
+    assert version_info.minor >= 8
+
+def test_requests_version():
+    assert requests_version() == "2.27.1"
+
+def test_pytest_version():
+    assert pytest_version() == "7.1.3"
